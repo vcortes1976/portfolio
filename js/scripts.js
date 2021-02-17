f95772cb46983b3e372f3be188bd7de6bbc298de
vcortes1976 <vcortes1976@gmail.com>
2021-02-17 15:39:54 +0000

    menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to my portfolio site! Feel free to get in touch if you like what you see.");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
