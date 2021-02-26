deb57802d9f7c0f417dfbe438dd018801c951653
Veronica Cortes <vcortes1976@gmail.com>
2021-02-26 18:29:58 +0000

    first iterations

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-console.log("Hi! Welcome to my portfolio site! Feel free to get in touch if you like what you see.");
+console.log("Hi! Welcome to my portfolio site!");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
